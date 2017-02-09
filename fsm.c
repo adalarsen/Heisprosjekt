366c9a343f446570e7d2c67fd971da5ee82079de
Jenny Bogen <jenny@dhcp-10-22-79-100.wlan.ntnu.no>
2017-02-09 19:57:27 +0100

    la til noe greier

---
+++ fsm.c
@@ -0,0 +1,27 @@
+#include "fsm.h"
+#include "hw.h"
+#include "elev.h"
+#include "queue.h"
+#include "timer.h"
+
+
+void fsm_elev_on(){
+
+}
+
+void fsm_floor_button_pressed(){
+
+}
+
+void fsm_elev_button_pressed(){
+
+}
+
+void fsm_is_on_floor(){
+
+}
+
+void fsm_stop_button_pressed(){
+
+}
+
