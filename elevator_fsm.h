e8723ac3f898f155435ed622599c6778ffd1bdcf
Jenny Bogen <jenny@dhcp-10-22-78-75.wlan.ntnu.no>
2017-02-16 19:23:22 +0100

    oppdaterte fsm.h

---
+++ elevator_fsm.h
@@ -0,0 +1,15 @@
+#include "hw.h"
+#include "elev.h"
+#include "queue.h"
+#include "timer.h"
+
+
+void fsm_elev_on();
+
+void fsm_floor_button_pressed(order_description new_order, );
+
+void fsm_elev_button_pressed();
+
+void fsm_is_on_floor();
+
+void fsm_stop_button_pressed();
