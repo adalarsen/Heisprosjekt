49f79181d1473702a31f08c815aa1fe631222d37
Jenny Bogen <jenny@dhcp-10-22-77-183.wlan.ntnu.no>
2017-02-23 19:56:18 +0100

    fikset litt

--- queue.h
+++ queue.h
@@ -10,19 +10,6 @@ void q_delete_all();
 
 order_description q_get_order();
 
-void q_store_order(order_description new_order); //lagrer en ny bestilling i koen 
+void q_store_order(int new_order); //lagrer en ny bestilling i koen 
 
-struct order_description{ //informerer om etasjen heisen skal til, hvilken etasje den sist passerte, og hvilken retning den hadde da den passerte.
-	int direction;
-	int last_floor_passed;
-	int next_floor_ordered;
-	int direction_ordered;
-	int order_origin_floor;
-	int ELEV_BUTTON_ORDER[3] = [next_floor_ordered, last_floor_passed, direction];
-	int FLOOR_BUTTON_ORDERED[4]=[direction_ordered, order_origin_floor, last_floor_passed, direction];
-};
 
-struct order_queue {
-	order_description orders[25];
-	void order_queue_init();
-};
