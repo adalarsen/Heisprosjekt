f2fe80be3097275845092153b2b18fa2ba114eb5
Jenny Bogen <jenny@dhcp-10-22-77-183.wlan.ntnu.no>
2017-02-23 19:55:13 +0100

    slettet nesten alt

--- elevator_fsm.c
+++ elevator_fsm.c
@@ -1,58 +1,29 @@
-#include "fsm.h"
-#include "hw.h"
 #include "elev.h"
 #include "queue.h"
-#include "timer.h"
-#include "queue.h"
+#include "elevator_fsm.h"
+
+enum ELEV_BUTTONS = {FIRST, SECOND, THIRD, FORTH, FIRST_UP, SECOND_UP, THIRD_UP, SECOND_DOWN, THRID_DOWN, FORTH_DOWN};
+enum DIRECTIONS = {UP = 1, DOWN = -1, STOP = 0};
+enum FLOOR = {NO_FLOOR, FIRST_FLOOR, SECOND_FLOOR} 
+enum NEXT_STATE = {STOP, CONTINUE};
+
+int current_floor = -1;
+int last_floor_passed = -1;
 
 void fsm_elev_on(){
-	int GROUND = 0;
-	elev.init();	
-	int correct_floor = q_check_floor(GROUND_FLOOR);
-	while (correct_floor == GROUND_FLOOR) {
-		hw_set_direction(-1);
-		correct_floor = q_check_floor(GROUND_FLOOR);
-	}
-	hw_set_direction(0);
+	q_delete_all();
 }
 
-void fsm_floor_button_pressed(){
-
+int fsm_update_floor(int floor){ //sjekker om man er i etasjen som er bestilt, sletter i saa fall bestillingen
+	last_floor_passed = current_floor;
+	current_floor = floor;	//
+	if (q_check_floor(current_floor)) {
+		q_delete_order(current_floor);
+		return STOP;
+	}
+	return CONTINUE;
 }
 
-void fsm_elev_button_pressed(){
-
-}
-
-void fsm_is_on_floor(int floor){
-	hw_set_floor_light(floor);
-	if (q_check_floor(floor) != floor) {
-		break;
-	}
-	hw_set_direction(0);
-	hw_open_door();
-	timer_on();
-	while (!timer_is_time_out()) {
-		
-	}
-	hw_close_door();		
-}
-
-void fsm_stop_button_pressed(int floor){
-	order_description temp_element = q_get_order(); //hent ut forste element fra queuen som trengs for at heisen skal huske hvor den er etter stoppknappen slippes
-	while (hw_get_stop_button_status()!=0) {
-		hw_set_direction(0);
-		hw_set_stop_button_light(1);
-		q_delete_all();
-		for (int i=GROUND_FLOOR; i<N_FLOORS; i++) {
-			hw_set_elev_light(i, 0);
-			hw_set_floor_button_light(i, 0);
-		}
-		if (floor != NO_FLOOR) {
-			hw_open_door();
-		}
-	}
-	q_store_order(temp_element);
-	hw_set_stop_button_light(0);
-	hw_close_door();
+void fsm_stop_button_pressed(){
+	q_delete_all();
 }
