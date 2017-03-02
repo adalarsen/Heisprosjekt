8da4e2bac08cf3235798427fb87715f47ca4d7f4
jennybg <jennybg@stud.ntnu.no>
2017-03-02 19:42:43 +0100

    Ser det ann

--- elevator_fsm.h
+++ elevator_fsm.h
@@ -2,15 +2,11 @@
 #include "queue.h"
 
 
-<<<<<<< HEAD
-void fsm_elev_on();
-
-int fsm_update_floor(int floor);
-
-void fsm_stop_button_pressed();
-
-=======
 void fsm_elev_on(); 					//initializes the queue
 int fsm_update_floor(int floor);		//updates the queue when a new floor is passed
 void fsm_stop_button_pressed();			//deletes all entries in the queue when the stop button is pushed
->>>>>>> 2b9a9aa5a71925efce4190c60667b58c250a9f4c
+
+enum ELEV_BUTTONS = {FIRST, SECOND, THIRD, FORTH, FIRST_UP, SECOND_UP, THIRD_UP, SECOND_DOWN, THRID_DOWN, FORTH_DOWN};
+enum DIRECTIONS = {UP = 1, DOWN = -1, STOP = 0};
+enum FLOOR = {NO_FLOOR, FIRST_FLOOR, SECOND_FLOOR=, THIRD_FLOOR, FOURTH_FLOOR}
+enum NEXT_STATE = {STOP, CONTINUE};
