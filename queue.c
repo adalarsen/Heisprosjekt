c08622db132f57bbdd39c916b0fc830f7437ed55
jennybg <jennybg@stud.ntnu.no>
2017-03-03 09:42:08 +0100

    fortsetter paa queue-filer

--- queue.c
+++ queue.c
@@ -1,34 +1,47 @@
 #include "queue.h"
 #include "elevator_fsm.h"
 
-int orders[10] = {0} //0-3: ordering buttons inside the elevator, 4-6: up buttons,7-9: down buttons
+int orders[10] = {0} //order queue. 0-3: order buttons inside the elevator, 4-6: up buttons,7-9: down buttons
 
-int q_check_floor(int current_floor) {				 //tar inn etasjen den er i, sjekker i orders om gjeldende etasje er bestilt
+
+
+/************************************
+*returns 1 if there is an order on current_floor
+************************************/
+int q_check_floor(int current_floor) {
 	switch(current_floor) {
 		case 1:
-			if (orders[0]==1 || orders[4]==1) {
+			if (orders[0]==1 || orders[4]==1) {			//check orders on first floor
 				return 1;
 			}
 			return 0;
 		case 2:
-			if (orders[1]==1 || orders[5]==1 || orders[7]==1) {
+			if (orders[1]==1 || orders[5]==1 || orders[7]==1) {	//check second floor
 				return 1;
 			}
 			return 0;
 		case 3:
-			if (orders[2]==1 || orders[6]==1 || orders[8]==1) {
+			if (orders[2]==1 || orders[6]==1 || orders[8]==1) {	//check third floor
 				return 1;
 			}
 			return 0;
 		case 4:
-			if (orders[3]==1 || orders[9]) {
+			if (orders[3]==1 || orders[9]) {			//check fourth floor
 				return 1;
 			}
+		
+		default:
 			return 0;
 	}
 }
 
-void q_delete_order(int current_floor){				//deletes all orders in a given floor
+
+
+
+/************************************
+*deletes all orders on current_floor
+************************************/
+void q_delete_order(int current_floor){					
 	switch(current_floor) {
 		case 1: 
             orders[0]=0;
@@ -53,77 +66,114 @@ void q_delete_order(int current_floor){				//deletes all orders in a given floor
 	}
 }
 
-void q_delete_all(){
+
+
+
+
+/************************************
+*delete all orders in the order queue
+************************************/
+void q_delete_all(){														
 	for (int i=0;i<10;i++) {
-		orders[i]=-1;
+		orders[i]=0;
 	}
 }
 
-int q_get_order(int current_floor, int current_direction){
-    if (hw_check_floor(current_floor)) {
-        if (orders[current_floor-1]) {
-            return current_floor; 
-        }
-        else if (current_direction && (current_floor != FOURTH_FLOOR)) {
-            if (orders[current_floor + 3]) {
-                return current_floor;
-            }
-        }
-        else if ((current_direction==-1) && (current_floor !=FIRST_FLOOR)) {
-            if (orders[current_floor + 6]) {
-                return current_floor;
-                }
-        }
-        else if (current_direction == 0) {
-            switch (current_floor) {
-                case 1:
-                    if (orders[0] || orders[4]) {
-                        return FIRST_FLOOR;
-                    }
-                case 2:
-                case 3:
-                    if (orders[current_floor + 3] || orders[current_floor + 6]) {
-                        return current_floor;
-                    }
-                case 4:
-                    if (orders[3] || orders[9]) {
-                        return FOURTH_FLOOR;
-                    }
-                }
-         }
-    }
-    
 
-    if (current_direction == 0) {
-            for (int i=0;i<10;i++) {
-            if(orders[i]==1){
-                switch(i) {
-                    case(1):
-                    case(5):
-                    case (7):
-                        return SECOND_FLOOR;
-                    case(0):
-                    case(4):
-                        return FIRST_FLOOR;
-                    case(2):
-                    case(6):
-                    case(8):
-                        return THIRD_FLOOR;
-                    case(3):
-                    case(9):
-                        return FOURTH_FLOOR;
-                }
-            }
-        }
-    return NO_FLOOR;
-    }
-    if (current_direction) {
 
+
+/***********************************
+*return the floor where the next order to be executed is
+*function will loop in main when the elevator reaches a floor, therefore it cannot be used if elevator is on NO_FLOOR (0) 
+***********************************/
+int q_get_order(int current_floor, int current_direction){    
+	if (q_check_floor(current_floor)) {					//check if there is an order on the elevator's current floor
+        	if (orders[current_floor-1]) {					//returns the current floor if there is an order from inside the elevator to stop on that floor
+            		return current_floor; 
+        } else if (current_direction && (current_floor != FOURTH_FLOOR)) {	//stop if there is an order from outside the elevator in the same direction as the current direction (up) 
+            	if (orders[current_floor + 3]) {
+                	return current_floor;
+            	}
+        } else if ((current_direction==-1) && (current_floor !=FIRST_FLOOR)) {	//stop if there is an order from outside the elevator in the same direction as the current direction (down) 
+            	if (orders[current_floor + 6]) {
+                	return current_floor;
+                }
+        } else if (current_direction == 0) {					//stop if the elevator is in idle and on a floor, and there is a new order on that floor 
+            	switch (current_floor) {
+                	case 1:
+                    		if (orders[0] || orders[4]) {
+                        		return FIRST_FLOOR;
+                    		}
+                	case 2:
+                	case 3:
+                    		if (orders[current_floor + 3] || orders[current_floor + 6]) {
+                        		return current_floor;
+                    		}
+                	case 4:
+                    		if (orders[3] || orders[9]) {
+                        		return FOURTH_FLOOR;
+                    		}
+                	}
+         	}
+    	}
+
+    	if (current_direction == 0) {						//search for a new order if the elevator is in idle and the order queue is empty
+            	for (int i=0;i<10;i++) {
+            		if(orders[i]==1){
+                		switch(i) {
+                    			case 0:
+                                        case 4:
+                                                return FIRST_FLOOR;
+					case 1:
+                    			case 5:
+                    			case 7:
+                        			return SECOND_FLOOR;
+                   			case 2:
+                    			case(6):
+                    			case(8):
+                        			return THIRD_FLOOR;
+                    			case(3):
+                    			case(9):
+                        			return FOURTH_FLOOR;
+                		}
+            		}
+        	}
+    		return NO_FLOOR;
+    	}
+    	
+	if (current_direction) {						//check up buttons
+		for (int i=4; i<7; i++) {
+			if (orders[i]==1) {
+				return i-3;					//return the floor where the up button is pushed
+			}
+		}	
+	}
+	
+	if (current_direction == -1) {						//check down buttons
+		for (int i=7; i<10; i++) {
+			if (orders[i]==1) {
+				return i-6;					//return the floor where the down button is pushed
+		}
+	}
 }
 
-    if (hw_check_floor(current_floor)) {
-        if current_direction    
 
-void q_store_order(int button_pressed){ //lagrer en ny bestilling i koen
-	orders[button_pressed] = 1;
+
+
+/************************************
+*stores a 1 (new order) in orders at index button_pressed
+************************************/
+void q_store_order(int button_pressed){ 			
+	orders[button_pressed] = 1;				//button_pressed is the index of an order in the order_buttons array in main
 }
+
+
+
+
+
+
+
+
+
+
+
--- queue.h
+++ queue.h
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 
-int q_check_floor(int current_floor);
+int q_check_floor(int current_floor);						//returns 1 if there is an order on current_floor
 
-void q_delete_order();
+void q_delete_order(int floor);							//deletes all orders on floor (set to 0)
 
-void q_delete_all();
+void q_delete_all();								//deletes every order (set to 0) in orders 
 
-int q_get_order(int current_floor, int current_direction);
+int q_get_order(int current_floor, int current_direction);			//returns which floor the next order is on
 
-void q_store_order(int new_order); //lagrer en ny bestilling i koen 
+void q_store_order(int new_order); 						//stores a new order in orders 
 
 
